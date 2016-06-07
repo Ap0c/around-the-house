00d58ce98486e9c351a11535153dc94dc65ed728
Ap0c <Ap0c@users.noreply.github.com>
2016-06-07 19:03:37 +0100

    Setup database connection.

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 # ----- Imports ----- #
 
 from flask import Flask
+import os.path
+from db import Database
 
 
 # ----- Setup ----- #
 
+# The database schema file.
+DB_SCHEMA = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+	'schema.sql')
+
+# The database file.
+DB_FILE = 'media.db'
+
 # The flask app object.
 app = Flask(__name__)
+
+# Handles database connections and queries.
+db = Database(DB_FILE, DB_SCHEMA)
 
 
 # ----- Routes ----- #
