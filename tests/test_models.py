ecb8fbbe8531d8e825b491962f760e3fe6513440
Ap0c <Ap0c@users.noreply.github.com>
2016-06-13 18:06:12 +0100

    Added tests for models search method, abstracted book mocking into separate function.

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,6 +4,15 @@ import unittest
 import book_db.models as models
 
 
+# ----- Functions ----- #
+
+def mock_book(title='Book One', author='Author One', location='Location One'):
+
+	"""Creates a mocked book in the database."""
+
+	return models.new_book(title, author, location), title, author, location
+
+
 # ----- Tests ----- #
 
 class TestModels(unittest.TestCase):
@@ -22,20 +31,18 @@ class TestModels(unittest.TestCase):
 
 		"""Checks if a book is added to the database."""
 
-		book_id = models.new_book('Book One', 'Author One', 'Location One')
+		book_id = mock_book()[0]
 		self.assertEqual(book_id, 1)
 
 	def test_retrieve_book(self):
 
 		"""Checks if a book is correctly retrieved from the database."""
 
-		title, author, location = 'Book One', 'Author One', 'Location One'
-
-		models.new_book(title, author, location)
-		book = models.book(1)
+		book_id, title, author, location = mock_book()
+		book = models.book(book_id)
 
 		self.assertIsNotNone(book)
-		self.assertEqual(book['docid'], 1)
+		self.assertEqual(book['docid'], book_id)
 		self.assertEqual(book['title'], title)
 		self.assertEqual(book['author'], author)
 		self.assertEqual(book['location'], location)
@@ -46,3 +53,22 @@ class TestModels(unittest.TestCase):
 
 		book = models.book(1)
 		self.assertIsNone(book)
+
+	def test_search_title(self):
+
+		"""Makes sure search returns title results."""
+
+		book_id = mock_book(title='Dummy Title', author='Made Up Author')[0]
+
+		book = models.search('Dummy')[0]
+		self.assertEqual(book['docid'], book_id)
+
+	def test_search_author(self):
+
+		"""Makes sure search returns author results."""
+
+		book_id = mock_book(title='Dummy Title', author='Made Up Author')[0]
+
+		book = models.search('Made')[0]
+		self.assertEqual(book['docid'], book_id)
+
