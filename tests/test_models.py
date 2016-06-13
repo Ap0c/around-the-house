681cd5d6aac53aa4a9d5bf180be914a8663723a5
Ap0c <Ap0c@users.noreply.github.com>
2016-06-13 17:40:01 +0100

    Added tests for model book retrieval.

--- tests/test_models.py
+++ tests/test_models.py
@@ -20,6 +20,29 @@ class TestModels(unittest.TestCase):
 
 	def test_new_book(self):
 
+		"""Checks if a book is added to the database."""
+
 		book_id = models.new_book('Book One', 'Author One', 'Location One')
 		self.assertEqual(book_id, 1)
 
+	def test_retrieve_book(self):
+
+		"""Checks if a book is correctly retrieved from the database."""
+
+		title, author, location = 'Book One', 'Author One', 'Location One'
+
+		models.new_book(title, author, location)
+		book = models.book(1)
+
+		self.assertIsNotNone(book)
+		self.assertEqual(book['docid'], 1)
+		self.assertEqual(book['title'], title)
+		self.assertEqual(book['author'], author)
+		self.assertEqual(book['location'], location)
+
+	def test_retrieve_no_book(self):
+
+		"""Checks if a book is not retrieved from the database."""
+
+		book = models.book(1)
+		self.assertIsNone(book)
